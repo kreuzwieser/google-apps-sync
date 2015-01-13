d37fc9b7372cb93ea8cb1e6e6b82e28ff60533f2
root <root@emu.ro.vutbr.cz>
2015-01-13 14:40:04 +0100

    better cache saving

--- Caching.pm
+++ Caching.pm
@@ -6,8 +6,11 @@ use strict;
 use JSON;
 use File::Path qw(make_path);
 use parent qw/Exporter/;
+use Monitoring;
 
 our @EXPORT = qw/load_scopes_from_cache save_scopes_to_cache load_scopes_from_cache_dir save_scopes_to_cache_dir get_data_from_json_file/;
+
+my $CACHE_DIRECTORY = "cache";
 
 sub load_scopes_from_cache
 {
@@ -25,18 +28,16 @@ sub save_scopes_to_cache
 
 sub load_scopes_from_cache_dir
 {
-    my ($dir) = @_;
-
     my $var = ();
-    foreach my $part (`ls -1 $dir`)
+    foreach my $part (`ls -1 $CACHE_DIRECTORY`)
     {
 	chomp $part;
-	foreach my $scope (`ls -1 $dir/$part`)
+	foreach my $scope (`ls -1 $CACHE_DIRECTORY/$part`)
 	{
 	    chomp $scope;
 	    if (($part eq "apps") && (($scope eq "members") || ($scope eq "photos") || ($scope eq "aliases")))
 	    {
-		foreach my $member (`ls -1 $dir/$part/$scope`)
+		foreach my $member (`ls -1 $CACHE_DIRECTORY/$part/$scope`)
 		{
 		    chomp $member;
 		    $var->{$part}->{$scope}->{$member} = load_scopes_from_cache("cache/$part/$scope/$member");
@@ -55,7 +56,7 @@ sub load_scopes_from_cache_dir
 
 sub save_scopes_to_cache_dir
 {
-    my ($apps_data, $dir) = @_;
+    my ($apps_data, $ts_start) = @_;
 
     foreach my $part (keys %{$apps_data})
     {
@@ -63,16 +64,41 @@ sub save_scopes_to_cache_dir
 	{
 	    if (($part eq "apps") && (($scope eq "members") || ($scope eq "photos")  || ($scope eq "aliases")))
 	    {
-		make_path("$dir/$part/$scope");
-		foreach my $member (keys %{$apps_data->{$part}->{$scope}})
+		make_path("$CACHE_DIRECTORY/$part/$scope");
+		foreach my $o (keys %{$apps_data->{$part}->{$scope}})
 		{
-		    save_scopes_to_cache($apps_data->{$part}->{$scope}->{$member}, "$dir/$part/$scope/$member");
+		    if (defined $apps_data->{$part}->{$scope}->{$o}->{last})
+		    {
+			if ($apps_data->{$part}->{$scope}->{$o}->{last} >= $ts_start)
+			{
+			    debug("Save $part -> $scope -> {$o} to cache", 'white');
+			    save_scopes_to_cache($apps_data->{$part}->{$scope}->{$o}, "$CACHE_DIRECTORY/$part/$scope/$o") 
+			}
+		    }
+		    else
+		    {
+			debug("Time $part -> $scope -> {$o} -> {last} is not defined, check it!", 'red');
+		    }
 		}
 	    }
 	    else
 	    {
-		make_path("$dir/$part");
-		save_scopes_to_cache($apps_data->{$part}->{$scope}, "$dir/$part/$scope");
+		make_path("$CACHE_DIRECTORY/$part");
+		my $last = $apps_data->{$part}->{$scope}->{last} if (defined $apps_data->{$part}->{$scope}->{last});
+		$last = $apps_data->{$part}->{$scope}->{1}->{last} if (defined $apps_data->{$part}->{$scope}->{1}->{last});
+		
+		if (defined $last)
+		{
+		    if ($last >= $ts_start)
+		    {
+			#debug("Save $part->$scope to cache", 'white');
+			save_scopes_to_cache($apps_data->{$part}->{$scope}, "$CACHE_DIRECTORY/$part/$scope");
+		    }
+		}
+		else
+		{
+		    debug("Time $part -> $scope -> {last} is not defined, check it!", 'red');
+		}
 	    }
 	}
     }
