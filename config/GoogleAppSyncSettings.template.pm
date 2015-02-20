121628c6b7c2a85f7a50b584b6ce82912023c2a2
Tomas Kreuzwieser <tomas@kreuzwieser.cz>
2015-02-20 23:57:14 +0100

    add password generator, passwords versioning

--- config/GoogleAppSyncSettings.template.pm
+++ config/GoogleAppSyncSettings.template.pm
@@ -1,15 +1,23 @@
 package GoogleAppSyncSettings;
 
+#use Crypt::CBC;
+#use MIME::Base64;
+#use Digest::MD5 qw(md5_base64);
+#use Digest::SHA1 qw(sha1_hex);
+#use Data::Dumper;
+
 # Google APPS settings
 my $APPS_CONFIG = {
-    APPS_OAUTHCONF   => 'config/ouath2service.json',                # path to the oauth2 configuration and with private key string
-    APPS_DOMAIN      => 'mydomain.cz',                              # primary APPS domain
-    APPS_USER        => 'google-admin@mydomain.cz',                 # login with admin permission, all APPS admins are excluded from synchronization
-    APPS_CUSTOMER    => 'C12345678',                                # for orgunits synchronziation
-    APPS_MAXRESULTS  => 500,                                        # maximum is 500 object in single request response
-    APPS_TRY_MAX     => 5,                                          # maximum number of tries for each request
-    APPS_UD_OUP      => "/Organization",                            # default orgunit path for users
-    APPS_UD_PASSHASH => "11aaaaaabbbbbbbbbbbbbbbbcccccccccccccc99", # default secret hash for users (SAML2 auth enabled)
+    APPS_OAUTHCONF   => 'config/ouath2service.json',                               # path to the oauth2 configuration and with private key string
+    APPS_DOMAIN      => 'mydomain.cz',                                             # primary APPS domain
+    APPS_USER        => 'google-admin@mydomain.cz',                                # login with admin permission, all APPS admins are excluded from synchronization
+    APPS_CUSTOMER    => 'C12345678',                                               # for orgunits synchronziation
+    APPS_MAXRESULTS  => 500,                                                       # maximum is 500 object in single request response
+    APPS_TRY_MAX     => 5,                                                         # maximum number of tries for each request
+    APPS_UD_OUP      => "/Organization",                                           # default orgunit path for users
+    APPS_PASSHASH    => sub { return "11aaaaaabbbbbbbbbbbbbbbbcccccccccccccc99" }, # hash generator
+    APPS_VERSHASH    => sub { return "1234567890" },                               # hash timestamp generator (for hash versioning)
+
 };
 
 # LDAP/AD
